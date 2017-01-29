489554e714dfff6f30f5ebbcc74fb7f0a24f1d44
Lawliet <jerry_howl_lawliet@hotmail.com>
2017-01-29 17:29:14 -0600

    Acomodado Correctamente
    Acomodado...

--- BannerST.py
+++ BannerST.py
@@ -1,1 +1,99 @@
-# -*- coding: utf-8 -*-# Página 1: http://patorjk.com/software/taag# Página 2: http://www.kammerl.de/ascii/AsciiSignature.php## Añadir una 'r' (significado de 'raw' = 'crudo') antes de una cadena,# al imprimir en pantalla tomará la cadena tal cual como esta escrita# ignorando los \n \t etc... import randomBanner1 = """                                                                               ███████╗ ██████╗ ██████╗ ████████╗███████╗██╗  ██╗████████╗          ██╔════╝██╔═══██╗██╔══██╗╚══██╔══╝██╔════╝╚██╗██╔╝╚══██╔══╝          ███████╗██║   ██║██████╔╝   ██║   █████╗   ╚███╔╝    ██║             ╚════██║██║   ██║██╔══██╗   ██║   ██╔══╝   ██╔██╗    ██║             ███████║╚██████╔╝██║  ██║   ██║   ███████╗██╔╝ ██╗   ██║             ╚══════╝ ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝   ╚═╝                                                                                                                                  By: LawlietJH"""#(Font: 'ANSI shadow' - Desde: Página 1)Banner2 = """                                                                                  ╔═╗┌─┐┬─┐╔╦╗┌─┐─┐ ┬┌┬┐                              ╚═╗│ │├┬┘ ║ ├┤ ┌┴┬┘ │                               ╚═╝└─┘┴└─ ╩ └─┘┴ └─ ┴                                                                                                               By: LawlietJH"""#(Font: 'Calvin S' - Desde: Página 1)Banner3 = r"""                                                                         _______  _____   ______ _______ _______ _     _ _______         |______ |     | |_____/    |    |______  \___/     |            ______| |_____| |    \_    |    |______ _/   \_    |                                                                   														  By: LawlietJH"""#(Font: 'Cyberlarge' - Desde: Página 1)Banner4 = """                                                                                                   :                                                                     .     t#,                               ,;                                  ;W    ;##W.   j.                       f#i                                  f#E   :#L:WE   EW,       GEEEEEEEL    .E#t             GEEEEEEEL           .E#f   .KG  ,#D  E##j      ,;;L#K;;.   i#W,   :KW,      L,;;L#K;;.          iWW;    EE    ;#f E###D.       t#E     L#D.     ,#W:   ,KG   t#E            L##Lffi f#.     t#iE#jG#W;      t#E   :K#Wfff;    ;#W. jWi    t#E           tLLG##L  :#G     GK E#t t##f     t#E   i##WLLLLt    i#KED.     t#E             ,W#i    ;#L   LW. E#t  :K#E:   t#E    .E#L         L#W.      t#E            j#E.      t#f f#:  E#KDDDD###i  t#E      f#E:     .GKj#K.     t#E          .D#j         f#D#;   E#f,t#Wi,,,  t#E       ,WW;   iWf  i#K.    t#E         ,WK,           G#t    E#t  ;#W:    t#E        .D#; LK:    t#E    t#E         EG.             t     DWi   ,KK:    fE          tt i       tDj    fE         ,                                    :                             :                                                                                														  By: LawlietJH"""#(Font: 'Def Leppard' - Desde: Página 1)Banner5 = r"""                                                                                 __    ___   ____  ______  ____ _   _ ______                  (( \  // \\  || \\ | || | ||    \\ // | || |                   \\  ((   )) ||_//   ||   ||==   )X(    ||                    \_))  \\_//  || \\   ||   ||___ // \\   ||                                                                														  By: LawlietJH"""#(Font: 'Double' - Desde: Página 1)Banner6 = """                                                                                                                                                      .M\"""bgd             MMP""MM""YMM                 mm               ,MI    "Y             P'   MM   `7                 MM               `MMb.      ,pW"Wq.`7Mb,od8 MM  .gP"Ya `7M'   `MF'mmMMmm               `YMMNq. 6W'   `Wb MM' "' MM ,M'   Yb  `VA ,V'    MM               .     `MM 8M     M8 MM     MM 8M""""""    XMX      MM               Mb     dM YA.   ,A9 MM     MM YM.    ,  ,V' VA.    MM               P"Ybmmd"   `Ybmd9'.JMML. .JMML.`Mbmmd'.AM.   .MA.  `Mbmo                                                                    														  By: LawlietJH"""#(Font: 'Georgia11' - Desde: Página 1)def Banner():    Banners = [ Banner1, Banner2, Banner3, Banner4, Banner5,\		Banner6 ]    return random.choice(Banners)
+# -*- coding: utf-8 -*-
+# Página 1: http://patorjk.com/software/taag
+# Página 2: http://www.kammerl.de/ascii/AsciiSignature.php
+#
+# Añadir una 'r' (significado de 'raw' = 'crudo') antes de una cadena,
+# al imprimir en pantalla tomará la cadena tal cual como esta escrita
+# ignorando los \n \t etc... 
+
+
+import random
+
+Banner1 = """
+                                                                     
+          ███████╗ ██████╗ ██████╗ ████████╗███████╗██╗  ██╗████████╗
+          ██╔════╝██╔═══██╗██╔══██╗╚══██╔══╝██╔════╝╚██╗██╔╝╚══██╔══╝
+          ███████╗██║   ██║██████╔╝   ██║   █████╗   ╚███╔╝    ██║   
+          ╚════██║██║   ██║██╔══██╗   ██║   ██╔══╝   ██╔██╗    ██║   
+          ███████║╚██████╔╝██║  ██║   ██║   ███████╗██╔╝ ██╗   ██║   
+          ╚══════╝ ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝   ╚═╝   
+                                                                     
+                                                          By: LawlietJH
+"""
+#(Font: 'ANSI shadow' - Desde: Página 1)
+
+
+Banner2 = """
+                                                    
+                              ╔═╗┌─┐┬─┐╔╦╗┌─┐─┐ ┬┌┬┐
+                              ╚═╗│ │├┬┘ ║ ├┤ ┌┴┬┘ │ 
+                              ╚═╝└─┘┴└─ ╩ └─┘┴ └─ ┴ 
+                                                    
+                                                          By: LawlietJH
+"""
+#(Font: 'Calvin S' - Desde: Página 1)
+
+Banner3 = r"""
+                                                                
+         _______  _____   ______ _______ _______ _     _ _______
+         |______ |     | |_____/    |    |______  \___/     |   
+         ______| |_____| |    \_    |    |______ _/   \_    |   
+                                                                
+														  By: LawlietJH
+"""
+#(Font: 'Cyberlarge' - Desde: Página 1)
+
+
+Banner4 = """
+                                                                             
+                      :                                                      
+               .     t#,                               ,;                    
+              ;W    ;##W.   j.                       f#i                     
+             f#E   :#L:WE   EW,       GEEEEEEEL    .E#t             GEEEEEEEL
+           .E#f   .KG  ,#D  E##j      ,;;L#K;;.   i#W,   :KW,      L,;;L#K;;.
+          iWW;    EE    ;#f E###D.       t#E     L#D.     ,#W:   ,KG   t#E   
+         L##Lffi f#.     t#iE#jG#W;      t#E   :K#Wfff;    ;#W. jWi    t#E   
+        tLLG##L  :#G     GK E#t t##f     t#E   i##WLLLLt    i#KED.     t#E   
+          ,W#i    ;#L   LW. E#t  :K#E:   t#E    .E#L         L#W.      t#E   
+         j#E.      t#f f#:  E#KDDDD###i  t#E      f#E:     .GKj#K.     t#E   
+       .D#j         f#D#;   E#f,t#Wi,,,  t#E       ,WW;   iWf  i#K.    t#E   
+      ,WK,           G#t    E#t  ;#W:    t#E        .D#; LK:    t#E    t#E   
+      EG.             t     DWi   ,KK:    fE          tt i       tDj    fE   
+      ,                                    :                             :   
+                                                                             
+														  By: LawlietJH
+"""
+#(Font: 'Def Leppard' - Desde: Página 1)
+
+
+Banner5 = r"""
+                                                              
+                   __    ___   ____  ______  ____ _   _ ______
+                  (( \  // \\  || \\ | || | ||    \\ // | || |
+                   \\  ((   )) ||_//   ||   ||==   )X(    ||  
+                  \_))  \\_//  || \\   ||   ||___ // \\   ||  
+                                                              
+														  By: LawlietJH
+"""
+#(Font: 'Double' - Desde: Página 1)
+
+
+Banner6 = """                                                                     
+                                                                    
+             .M\"""bgd             MMP""MM""YMM                 mm   
+            ,MI    "Y             P'   MM   `7                 MM   
+            `MMb.      ,pW"Wq.`7Mb,od8 MM  .gP"Ya `7M'   `MF'mmMMmm 
+              `YMMNq. 6W'   `Wb MM' "' MM ,M'   Yb  `VA ,V'    MM   
+            .     `MM 8M     M8 MM     MM 8M""""""    XMX      MM   
+            Mb     dM YA.   ,A9 MM     MM YM.    ,  ,V' VA.    MM   
+            P"Ybmmd"   `Ybmd9'.JMML. .JMML.`Mbmmd'.AM.   .MA.  `Mbmo
+                                                                    
+														  By: LawlietJH
+"""
+#(Font: 'Georgia11' - Desde: Página 1)
+
+
+def Banner():
+    Banners = [ Banner1, Banner2, Banner3, Banner4, Banner5,\
+		 Banner6 ]
+    return random.choice(Banners)
